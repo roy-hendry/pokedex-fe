878bf6f4ee8dcfe7739994cfb1e64eb527ddf7b6
roy-hendry <royjairusraj@hotmail.com>
2023-01-18 19:20:07 +0000

    All Pokemon are now being pulled from the API successfully

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
-function Card({ pokemonId, pokemonName, spriteUrl }) {
+function Card({ pokemonData }) {
 	return (
 		<div className="card">
-			<h2>{pokemonId}</h2>
-			<h2>{pokemonName}</h2>
-			<img src={spriteUrl} alt="" />
+			<h2>{pokemonData.id}</h2>
+			<h2>{pokemonData.name}</h2>
+			<img src={pokemonData.sprites.front_default} alt="Pokemon" />
 		</div>
 	);
 }
