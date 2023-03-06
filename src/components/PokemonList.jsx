43aa69c84520774a1d01044218003f030481608a
roy-hendry <royjairusraj@hotmail.com>
2023-03-06 21:23:24 +0000

    Changed PokemonList to be written without 2 returns

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,15 +1,11 @@
 import Card from "../components/Card";
 
 function PokemonList({ pokemonData }) {
-	console.log(pokemonData);
-	return pokemonData.map((item) => {
-		console.log(item.id);
-		return (
-			<div key={item.id}>
-				<Card pokemonData={item} />
-			</div>
-		);
-	});
+	return pokemonData.map((item) => (
+		<div key={item.id}>
+			<Card pokemonData={item} />
+		</div>
+	));
 }
 
 export default PokemonList;
