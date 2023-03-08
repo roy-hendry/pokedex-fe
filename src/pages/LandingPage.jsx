56420bfa39e0f56908179ad78a1a824d778d20d3
roy-hendry <royjairusraj@hotmail.com>
2023-03-08 21:00:18 +0000

    Pagination Added

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import { getPokemonData } from "../services/pokemonServices";
 import PokemonInfo from "../components/PokemonInfo";
 import PokemonList from "../components/PokemonList";
-import Card from "../components/Card";
 
 const LandingPage = () => {
 	const [loading, setLoading] = useState(false);
 	const [allPokemonData, setAllPokemonData] = useState([]);
+	const [pageNumber, setPageNumber] = useState(1);
 
 	useEffect(() => {
 		setLoading(true);
@@ -24,15 +24,33 @@ const LandingPage = () => {
 			{!loading && (
 				<div className="container">
 					<div className="left-content">
-						{/* {allPokemonData.map((item) => {
-							return (
-								<div key={item.id}>
-									<Card pokemonData={item} />
-								</div>
-							);
-						})} */}
-						{/* console.log(pokemonData) */}
-						<PokemonList pokemonData={allPokemonData} />
+						<PokemonList
+							pokemonData={allPokemonData.slice(
+								(pageNumber - 1) * 20,
+								pageNumber * 20
+							)}
+						/>
+						<div className="btn-group">
+							{pageNumber > 1 && (
+								<button
+									onClick={() => {
+										setPageNumber(pageNumber - 1);
+									}}
+								>
+									Previous
+								</button>
+							)}
+
+							{pageNumber < 8 && (
+								<button
+									onClick={() => {
+										setPageNumber(pageNumber + 1);
+									}}
+								>
+									Next
+								</button>
+							)}
+						</div>
 					</div>
 					<div className="right-content">
 						<PokemonInfo />
