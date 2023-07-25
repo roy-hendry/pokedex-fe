810f45e9b01e15f1d3dd0ecb9f2a4bb4be090908
roy-hendry <royjairusraj@hotmail.com>
2023-07-25 18:33:34 +0100

    Added detailed sprites to view

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -1,10 +1,16 @@
 function PokemonInfo({ pokemonData }) {
-	let sqlInsertStatement = "";
+	const paddedId = pokemonData.id.toString().padStart(3, "0");
+
+	let sqlInsertStatement = paddedId + ",\n";
 	const n =
 		pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
 
 	sqlInsertStatement += "'" + n + "',\n";
 	sqlInsertStatement += "'" + pokemonData.sprites.front_default + "',\n";
+	sqlInsertStatement +=
+		"'https://assets.pokemon.com/assets/cms2/img/pokedex/full/" +
+		paddedId +
+		".png',\n";
 	const a =
 		pokemonData.abilities[0].ability.name.charAt(0).toUpperCase() +
 		pokemonData.abilities[0].ability.name.slice(1);
@@ -15,7 +21,7 @@ function PokemonInfo({ pokemonData }) {
 			pokemonData.abilities[1].ability.name.slice(1);
 		sqlInsertStatement += "'" + b + "',\n";
 	} else {
-		sqlInsertStatement += ",\n";
+		sqlInsertStatement += "null,\n";
 	}
 	sqlInsertStatement += pokemonData.stats[0].base_stat + ",\n";
 	sqlInsertStatement += pokemonData.stats[1].base_stat + ",\n";
@@ -24,8 +30,7 @@ function PokemonInfo({ pokemonData }) {
 	sqlInsertStatement += pokemonData.stats[4].base_stat + ",\n";
 	sqlInsertStatement += pokemonData.stats[5].base_stat;
 
-	// console.log(sqlInsertStatement);
-	const paddedId = pokemonData.id.toString().padStart(3, "0");
+	console.log(sqlInsertStatement);
 
 	return (
 		pokemonData && (
